ce4c0573b561febca18fa209187f06d9ee5cf6af
Jared Volpe <plainspace@gmail.com>
2016-02-13 10:15:45 -0800

    Replace TermsViewController

--- Carousel/TermsViewController.swift
+++ Carousel/TermsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class TermsViewController: UIViewController {
-
+    
     @IBOutlet weak var termsWebView: UIWebView!
     
     @IBAction func didTapDone(sender: AnyObject) {
@@ -30,21 +30,21 @@ class TermsViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
